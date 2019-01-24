a8a603004bba9cfc600d3194927dd4b56136175a
Gilberto Magalhães Leite Filho <gilbertomagalhaeslf@gmail.com>
2019-01-24 20:42:47 -0200

    Incluindo script que chama o endpoint e exibe as contagens

--- fizzbuzz_caller.py
+++ fizzbuzz_caller.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+import requests
+
+
+ANSWERS = []
+EVEN_COUNT = 0
+ODD_COUNT = 0
+FIZZ_COUNT = 0
+BUZZ_COUNT = 0
+FIZZBUZZ_COUNT = 0
+
+
 def fizzbuzz_calculator(number):
     word = ''
     if not number % 3:
@@ -7,3 +18,38 @@ def fizzbuzz_calculator(number):
         word += 'Buzz'
     print(word or number)
     return word or str(number)
+
+
+def fizzbuzz_call(number):
+    request = requests.get('http://127.0.0.1:8000/' + str(number))
+    answer = request.json().get('answer')
+    print(answer)
+    return answer
+
+
+for number in range(100):
+    answer = fizzbuzz_call(number + 1)
+    ANSWERS.append(answer)
+
+    if answer == 'Fizz':
+        FIZZ_COUNT += 1
+    elif answer == 'Buzz':
+        BUZZ_COUNT += 1
+    elif answer == 'FizzBuzz':
+        FIZZBUZZ_COUNT += 1
+    else:
+        try:
+            parsed = int(answer)
+            if parsed % 2 == 0:
+                EVEN_COUNT += 1
+            else:
+                ODD_COUNT += 1
+        except:
+            print('Bad answer')
+
+print('Números pares:', EVEN_COUNT)
+print('Números ímpares:', ODD_COUNT)
+print('Fizz:', FIZZ_COUNT)
+print('Buzz:', BUZZ_COUNT)
+print('FizzBuzz:', FIZZBUZZ_COUNT)
+print(' '.join(ANSWERS))
